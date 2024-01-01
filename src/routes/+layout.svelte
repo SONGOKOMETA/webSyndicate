c17457fd3cdad04f13bec6fd0c782e74db75cb3e
BranoGojkovic <branjevo1995@yandex.ru>
2024-01-01 05:41:54 +0100

    Added basic routing, nav WIP

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+  import "../styles/navStyle.css";
+</script>
+
+<div class="nav-Wrapper">
+  <div class="nav-Wrapper-Body">
+    <h1 id="main-Text"><a href="/">Syndicate.</a></h1>
+    <nav id="sub-Text">
+      <a href="/corp1">Corp1</a>
+      <a href="/corp2">Corp2</a>
+      <a href="/corp3">Corp3</a>
+    </nav>
+  </div>
+</div>
+
+<slot />
