6209a93debbc673f871e666cf5c80ed5e1ad135f
BranoGojkovic <branjevo1995@yandex.ru>
2024-01-02 00:25:36 +0100

    Finished navbar

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,27 @@
 
 <div class="nav-Wrapper">
   <div class="nav-Wrapper-Body">
-    <h1 id="main-Text"><a href="/">Syndicate.</a></h1>
-    <nav id="sub-Text">
-      <a href="/corp1">Corp1</a>
-      <a href="/corp2">Corp2</a>
-      <a href="/corp3">Corp3</a>
-    </nav>
+    <div class="nav-Wrapper-Body-Left">
+      <img src="src/static/synd-logo.png" alt="synd-logo.png" />
+      <h1 id="main-Text"><a href="/">Syndicate.</a></h1>
+      <nav id="sub-Text">
+        <a href="/corp1">RD13X</a>
+        <a href="/corp2">RXSK</a>
+        <a href="/corp3">PRGCO</a>
+      </nav>
+    </div>
+    <div class="nav-Wrapper-Body-Right">
+      <a href="/">
+        <div class="seat-Button">
+          <div class="seat-Button-Content">
+            <img src="src/static/seat-logo.png" alt="seat-logo" />
+            <span>
+              S<b>e</b>AT
+            </span>
+          </div>
+        </div>
+      </a>
+    </div>
   </div>
 </div>
 
