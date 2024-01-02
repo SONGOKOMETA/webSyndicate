c5aef15f4523ec4c68bc043f60a643ff91fd5766
BranoGojkovic <branjevo1995@yandex.ru>
2024-01-02 01:33:18 +0100

    Created midSection skeleton and styling

---
+++ src/headers/midSection.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../styles/midSectionStyle.css";
+</script>
+
+<div class="main-Container">
+  <h2>Syndicate. Activities</h2>
+  <br />
+  <span
+    >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis
+    tincidunt rutrum:
+  </span>
+  <br />
+  <br />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script>
   import TopSection from "../headers/topSection.svelte";
+  import MidSection from "../headers/midSection.svelte";
 </script>
 
 <TopSection />
+<MidSection />
