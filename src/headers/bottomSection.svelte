4e05891c5d642e9c92b630e1d9085f8843d0dae2
BranoGojkovic <branjevo1995@yandex.ru>
2024-01-02 02:23:47 +0100

    created bottomSection and styling for it

---
+++ src/headers/bottomSection.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../styles/bottomSectionStyle.css";
+</script>
+
+<div class="main-Container">
+  <h1>Join Syndicate. Today!</h1>
+  <a href="/">
+    <div class="main-Container-Discord-Button_bottom">
+      <img src="src/static/discord-logo.png" alt="discord logo" />
+      <span>Join the Syndicate. Discord</span>
+    </div>
+  </a>
+  <br />
+</div>
