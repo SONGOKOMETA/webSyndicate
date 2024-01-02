6142d50b540affc49ff3a5118a22712edf6a1461
BranoGojkovic <branjevo1995@yandex.ru>
2024-01-02 02:44:16 +0100

    Created gallery in midSection

--- src/headers/midSection.svelte
+++ src/headers/midSection.svelte
@@ -4,72 +4,29 @@
 
 <div class="main-Container">
   <h2>Syndicate. Activities</h2>
-  <br />
-  <span
+  <span id="flavor-Text_mid"
     >Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis
     tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit.
     Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur
-    adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit
-    amet, consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem
-    ipsum dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum: Lorem ipsum dolor sit amet, consectetur adipiscing
-    elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum dolor sit amet,
-    consectetur adipiscing elit. Vivamus iaculis tincidunt rutrum: Lorem ipsum
-    dolor sit amet, consectetur adipiscing elit. Vivamus iaculis tincidunt
-    rutrum:Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus
-    iaculis tincidunt rutrum:
+    adipiscing elit.
   </span>
   <br />
   <br />
+  <div class="mid-Gallery">
+    <div class="mid-Gallery_wrapper">
+      <div class="mid-Gallery_content">topleft</div>
+      <div class="mid-Gallery_content">topmid</div>
+      <div class="mid-Gallery_content">topright</div>
+    </div>
+    <div class="mid-Gallery_wrapper">
+      <div class="mid-Gallery_content">midleft</div>
+      <div class="mid-Gallery_content">midmid</div>
+      <div class="mid-Gallery_content">midright</div>
+    </div>
+    <div class="mid-Gallery_wrapper">
+      <div class="mid-Gallery_content">botleft</div>
+      <div class="mid-Gallery_content">botmid</div>
+      <div class="mid-Gallery_content">botright</div>
+    </div>
+  </div>
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,9 @@
 <script>
   import TopSection from "../headers/topSection.svelte";
   import MidSection from "../headers/midSection.svelte";
+  import BottomSection from "../headers/bottomSection.svelte";
 </script>
 
 <TopSection />
 <MidSection />
+<BottomSection />
